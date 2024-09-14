284dd06ebe961e4d4cce3d3444566f680630610f
Tom Laird-McConnell <thermous@iciclecreek.com>
2024-09-14 13:50:20 -0700

    Add Compare operator extension to ITransformer

--- source/Linq.AI.OpenAI.Tests/Samples.cs
+++ source/Linq.AI.OpenAI.Tests/Samples.cs
@@ -109,57 +109,6 @@ namespace Linq.AI.OpenAI.Tests
             Assert.AreEqual(true, options.NoLogo);
         }
 
-        private static string GetItemText(object item1, object item2)
-        {
-            string item1Text = (item1 is string) ? item1 as string : JToken.FromObject(item1).ToString();
-            string item2Text = (item2 is string) ? item2 as string : JToken.FromObject(item2).ToString();
-            return $$"""
-                <ITEM1>
-                {{item1Text}}
-                
-                <ITEM2>
-                {{item2Text}}
-                """;
-        }
-
-        /// <summary>
-        /// Compare two objects for semantic equivelence using OpenAI model
-        /// </summary>
-        /// <param name="source">object to inspect</param>
-        /// <param name="transformer">ChatClient for model</param>
-        /// <param name="question">question you want answered</param>
-        /// <param name="instructions">system instructions to help control the answer</param>
-        /// <param name="cancellationToken">cancellation token to cancel the operation.</param>
-        /// <returns>answer of question</returns>
-        public bool CompareSemantically(object source, object target, ITransformer transformer, string? goal = null, string? instructions = null, CancellationToken cancellationToken = default)
-            => GetItemText(source, target).TransformItem<bool>(transformer, goal ?? "are <ITEM1> and <ITEM2> semantically equivelent", instructions, cancellationToken);
-
-        [TestMethod]
-        public void Compare_Strings_Semantically()
-        {
-            Assert.IsTrue(CompareSemantically("fourteen", "14", Model));
-            Assert.IsTrue(CompareSemantically("fourteen years old", "10 + 4 years", Model));
-            Assert.IsTrue(CompareSemantically("Me llamo Tom", "Mi nombre es Tom", Model));
-            Assert.IsTrue(CompareSemantically("My name is Tom", "Mi nombre es Tom", Model, instructions: "allow different langauges to be semantically equal"));
-            Assert.IsFalse(CompareSemantically("Me llamo Tom", "Mi padre es Tom", Model));
-        }
-
-        [Ignore]
-        [TestMethod]
-        public async Task Compare_Objects_Semantically()
-        {
-            var task1= Text.SelectAsync<Article>(Model);
-            var task2 = Text.SelectAsync<Article>(Model);
-            await Task.WhenAll(task1, task2);
-            var models1 = await task1;
-            var models2 = await task2;
-            for (int i = 0; i < models1.Count(); i++)
-            {
-                var model1 = models1[i];
-                var model2 = models2[i];
-                Assert.IsTrue(CompareSemantically(model1, model2, Model));
-            }
-        }
     }
 
 }
---
+++ source/Linq.AI/CompareExtension.cs
@@ -0,0 +1,50 @@
+﻿
+using Iciclecreek.Async;
+using Newtonsoft.Json;
+
+namespace Linq.AI
+{
+
+    
+    public static class CompareExtension
+    {
+        /// <summary>
+        /// Compare 2 objects for semantic equivelancy
+        /// </summary>
+        /// <param name="transformer">transformer to use</param>
+        /// <param name="item1">item1 to compare</param>
+        /// <param name="item2">item2 to compare</param>
+        /// <param name="instructions">instructions for how to compare</param>
+        /// <param name="cancellationToken">cancellationToken</param>
+        /// <returns>true/false</returns>
+        public static bool Compare(this ITransformer transformer, object item1, object item2, string instructions = null, CancellationToken cancellationToken = default)
+            => transformer.CompareAsync(item1, item2, instructions, cancellationToken).Result;
+
+        /// <summary>
+        /// Compare 2 objects for semantic equivelancy async
+        /// </summary>
+        /// <param name="transformer">transformer to use</param>
+        /// <param name="item1">item1 to compare</param>
+        /// <param name="item2">item2 to compare</param>
+        /// <param name="instructions">instructions for how to compare</param>
+        /// <param name="cancellationToken">cancellationToken</param>
+        /// <returns>true/false</returns>
+        public static Task<bool> CompareAsync(this ITransformer transformer, object item1, object item2, string instructions = null, CancellationToken cancellationToken = default)
+        {
+            item1 = item1 ?? String.Empty;
+            item2 = item2 ?? String.Empty;
+            string item1Text = ((item1 is string) ? item1 as string : JsonConvert.SerializeObject(item1))!;
+            string item2Text = ((item2 is string) ? item2 as string : JsonConvert.SerializeObject(item2))!;
+            return $"""
+			<ITEM1>
+			{item1Text}
+			
+			<ITEM2>
+			{item2Text}
+			"""
+                .TransformItemAsync<bool>(transformer, "are <ITEM1> and <ITEM2> semantically equivelent?", instructions, cancellationToken);
+        }
+
+    }
+}
+
